bacc5c4335cab88e16dc8a47e6898d4766bdba1f
mayank-gupta-03 <gupta.mayank2003@gmail.com>
2026-05-30 15:25:36 +0530

    feat: add workspace service scaffold for build engine

---
+++ backend/src/main/java/com/forgedeploy/service/modules/deployments/service/WorkspaceService.java
@@ -0,0 +1,8 @@
+package com.forgedeploy.service.modules.deployments.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WorkspaceService {
+    private static String BASE_PATH = "/tmp/forgedeploy/workspaces/";
+}
